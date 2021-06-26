1e69dec3afbc4a94da6f7167eec23b355f1b2748
Kchhabra14 <khushichhabra716@gmail.com>
2021-06-27 02:52:17 +0530

    drew frame on results

--- social-distance-detector.py
+++ social-distance-detector.py
@@ -100,6 +100,25 @@ while True:
       #Adding the resulant boxes to final results
       r = (classids[i],confidences[i], (x, y, x + w, y + h), centroids[i])
       results.append(r)  
+      print(results)
+
+  #Function to draw frame using the coordinates appended
+    #loop over the results
+  for (index,(classid,prob,bbox,centeriod)) in enumerate(results):
+    #extract the bounding box and centroid coordinates 
+    (startx,starty,endx,endy) =bbox
+    #setup the color of the annotation
+    color=(0,255,0)
+    #grabbing the centroid 
+    (cx,cy) = centeriod
+    #grabbing the class name of the prediction 
+    label=str(classes[classid])
+    #draw a bounding box around the detected objects
+    cv2.rectangle(frame,(startx,starty),(endx,endy),color,2)
+    #draw circle around centroid coordinates of the person
+    cv2.circle(frame,(cx,cy),5,color,1)
+    #insert text according to label and confidences
+    cv2.putText(frame,label+"  "+str(int(prob*100))+"%",(startx,starty-10),cv2.FONT_HERSHEY_COMPLEX,0.75,color,2)
 
   # Display the resulting frame
   cv2.imshow('Frame', frame)
