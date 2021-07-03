442385a3589b2d2b31224d4921445d5950a6a471
danny <dcubedacosta@gmail.com>
2021-07-04 00:53:09 +0530

    grabs results after sending frame into network

--- social-distance-detector.py
+++ social-distance-detector.py
@@ -1,9 +1,9 @@
-# importing libraries
+# importing libraries and required files
 import cv2
 import numpy as np
 from config import *
 from boundingboxes import *
-
+from detect import *
 
 #Loading YOLOv4-tiny configuration file and weights 
 #Initializing network
@@ -14,7 +14,6 @@ classes = []
 with open(Modelclasses,"r") as file:
     classes=[eachline.strip() for eachline in file.readlines()]
 
-
 #Setting backend as OpenCV
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 
@@ -23,6 +22,7 @@ net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
 #Grabbing Layer Names from the Network
 layernames = net.getLayerNames()
+
 #Grabbing Output Layer Names from the Network
 outputlayers = [layernames[index[0]-1] for index in net.getUnconnectedOutLayers()]
 
@@ -39,78 +39,22 @@ modheight = int(originalheight * ratio)
 
 # Read until video is completed
 while True:
+  
   # Capture frame-by-frame
   cap,frame = videostream.read()
 
-  #Resizes the current frame to 800 pixels width
-  frame = cv2.resize(frame,(modwidth,modheight),interpolation=cv2.INTER_AREA)
-  results = []
-
-  #Converting current frame to blob to pass thorugh network
-  blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (320, 320),swapRB=True, crop=False)
-	
-  #Pass the blob through the network
-  net.setInput(blob)
-	#grabbing output layers with predictions
-  layerOutputs = net.forward(outputlayers)
-
-	#Intializing Lists for bounding boxes , labels , centriods and confidences of each prediction
-  boxes = []
-  classids = []
-  centroids = []
-  confidences = []
-
-	#Looping over each output layer
-  for output in layerOutputs:
-    
-    for detection in output:
-
-			#Extracting prediction values
-      scores = detection[5:]
-			#Extracting the classid of the highest score
-      classID = np.argmax(scores)
-			#Storing the confidence of highest scoring index
-      confidence = scores[classID]
-
-			#filtering off weak predictions
-      if confidence > confidencethreshold:
-
-        #Storing the Border box for the detection
-        box = detection[0:4] * np.array([modwidth, modheight, modwidth, modheight])
-        (centerX, centerY, width, height) = box.astype("int")
-
-        #Calculating the values for the centriod of the borderbox
-        x = int(centerX - (width / 2))
-        y = int(centerY - (height / 2))
-
-        #Adding the values of the borderboxes,centroids, confidences and classids   
-        boxes.append([x, y, int(width), int(height)])
-        centroids.append((centerX, centerY))
-        confidences.append(float(confidence))
-        classids.append(classID)
-  #Applying Non maximum suppression to select one borderbox per perdiction
-  finalindexs = cv2.dnn.NMSBoxes(boxes, confidences,confidencethreshold,nmsthreshold)
+  #Grabs results after sending it into the network
+  results = detect(net,frame,modwidth,modheight,outputlayers)
   
-  if len(finalindexs) > 0:
-    for i in finalindexs.flatten():
-      
-      #Grabbing the coordinates of the primary box
-      (x, y) = (boxes[i][0], boxes[i][1])
-      (w, h) = (boxes[i][2], boxes[i][3])
-
-      #Adding the resulant boxes to final results
-      r = (classids[i],confidences[i], (x, y, x + w, y + h), centroids[i])
-      results.append(r)  
-      print(results)
-  frame=bboxes(results,classes,frame)
+  #Draws bounding boxes on each frame 
+  frame = bboxes(results,classes,frame)
+ 
   # Display the resulting frame
   cv2.imshow('Frame', frame)
    
-    # Press q on keyboard to  exit
+  # Press q on keyboard to  exit
   if cv2.waitKey(1) & 0xFF == ord('q'):
     break
-  
-
 
 # When everything done, release 
 # the video capture object
